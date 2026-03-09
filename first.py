ecc365161eb10fe6c60a3de03360b6092460a00e
Achhita yadav <achhitayadav2407@gmail.com>
2026-03-09 18:03:27 +0530

    my third code

--- first.py
+++ first.py
@@ -1,2 +1,90 @@
 print("hello world")
 print("hello")
+
+# Write a program to print the first element of the list:
+list = [12, 24, 36, 48, 60]
+print(list[1])
+
+# Write a program to print the last element of the list:
+colors = ["red", "blue", "green", "yellow"]
+print(colors[3])
+
+
+#Write a program to find the length of the following list:
+names = ["Aman", "Riya", "Rahul", "Sneha"]
+print(len(names))
+
+# Write a program to add "grape" to the end of the list:
+fruits = ["apple", "banana", "mango"]
+fruits.append("grapes")
+print(fruits)
+
+# Write a program to insert "cat" at index 2 in the list:
+animals = ["dog", "lion", "tiger"]
+animals.insert(2, "cat")
+print(animals)
+
+# Write a program to remove "banana" from the list:
+fruits = ["apple", "banana", "mango", "orange"]
+fruits.remove("banana")
+print(fruits)
+
+
+# Write a program to check whether "apple" exists in the list:
+fruits = ["banana", "mango", "apple"]
+print(fruits.index("apple"))
+
+
+# Write a program to print all elements of the list using a for loop:
+numbers = [1, 2, 3, 4, 5]
+for i in numbers:
+    print(i)
+
+
+# Create a list named numbers containing 5 integers and print it.
+numbers = [2, 3, 4, 5, 6, 8]
+print(numbers)
+
+# Write a program to print the first element of the list.
+lst = [20, 30, 40, 50, 60]
+print(lst[1])
+
+# Write a program to print the last element of the list.
+lst = [20, 30, 40, 50, 60]
+print(lst[4])
+
+# Write a program to find the length of a list.
+lst = [20, 30, 40, 50, 60]
+print(len(lst))
+
+# Write a program to add an element at the end of a list.
+lst = [20, 30, 40, 50, 60]
+lst.append(80)
+print(lst)
+
+# Write a program to insert an element at a specific index in a list.
+lst = [20, 30, 40, 50, 60]
+lst.insert(3, 100)
+print(lst)
+
+# Write a program to remove an element from a list.
+lst = [20, 30, 40, 50, 60]
+lst.remove(40)
+print(lst)
+
+# Write a program to check whether an element exists in a list.
+lst = [20, 30, 40, 50, 60]
+print(lst.index(40))
+
+# Write a program to print all elements of a list using a for loop.
+lst = [20, 30, 40, 50, 60]
+for i in lst:
+    print(i)
+
+# Write a program to create an empty list and add five numbers to it.
+list = []
+for i in range(6):
+    num = int(input("enter any number: "))
+    list.append(num)
+print(list)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+list = []
+for i in range(6):
+    num = int(input("enter any number: "))
+    list.append(num)
+print(list)
